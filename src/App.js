e4492e23666fc7cedec238022bf584f896e0895b
Vitali Undra <vitalyundro@gmail.com>
2022-09-24 15:52:02 +0300

    init: complete task 4 frontend-part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Registration } from './pages/Registration.jsx';
+import { Users } from './pages/Users.jsx';
+import { useDispatch } from 'react-redux';
+import { fetchAuthMe } from './redux/slices/auth';
+import { useEffect } from 'react';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchAuthMe());
+	}, []);
+
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Users />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/registration' element={<Registration />} />
+				<Route path='/users' element={<Users />} />
+			</Routes>
+			<ToastContainer position='top-right' />
+		</>
+	);
 }
 
 export default App;
